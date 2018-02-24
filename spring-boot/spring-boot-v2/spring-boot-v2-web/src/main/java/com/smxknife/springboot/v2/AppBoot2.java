188c97512bb2396c0040153fdcf140ce7254f95b
smxknife <2323937771@qq.com>
2018-02-24 23:36:42 +0800

    SpringApplicationBuilder.child

--- spring-boot/spring-boot-v2/spring-boot-v2-web/src/main/java/com/smxknife/springboot/v2/AppBoot2.java
+++ spring-boot/spring-boot-v2/spring-boot-v2-web/src/main/java/com/smxknife/springboot/v2/AppBoot2.java
@@ -1,13 +1,13 @@
 package com.smxknife.springboot.v2;
 
 import com.smxknife.springboot.v2.config.AppConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class AppBoot2 {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(AppConfig.class)
+                .child()
                 .banner(new AppBanner())
                 .run(args);
     }
