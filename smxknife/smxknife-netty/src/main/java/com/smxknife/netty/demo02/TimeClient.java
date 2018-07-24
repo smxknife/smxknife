f7256445801b8ebb1eeba0145300256369da66c2
smxknife <2323937771@qq.com>
2018-07-24 09:31:05 +0800

    netty

---
+++ smxknife/smxknife-netty/src/main/java/com/smxknife/netty/demo02/TimeClient.java
@@ -0,0 +1,63 @@
+package com.smxknife.netty.demo02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TimeClient {
+	public static void main(String[] args) {
+		int port = 8080;
+
+		if (args != null && args.length > 0) {
+			port = Integer.valueOf(args[0]);
+		}
+
+		Socket socket = null;
+		BufferedReader in = null;
+		PrintWriter out = null;
+
+		try {
+			socket = new Socket("127.0.0.1", port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+
+			out.println("TIME ORDER");
+
+			System.out.println("send to server success");
+
+			String resp = in.readLine();
+
+			System.out.println("Now is : " + resp);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null) {
+				out.close();
+				out = null;
+			}
+
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				in = null;
+			}
+
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				socket = null;
+			}
+		}
+	}
+}
