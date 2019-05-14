fb50c139d1c0cbac7facc656e1f4ac48446c0da7
smxknife <2323937771@qq.com>
2019-05-14 10:46:43 +0800

    选择算法原理

---
+++ smxknife/smxknife-utils/src/main/java/com/smxknfie/utils/Stopwatch.java
@@ -0,0 +1,19 @@
+package com.smxknfie.utils;
+
+/**
+ * @author smxknife
+ * 2019-05-10
+ */
+public class Stopwatch {
+
+	private final long start;
+
+	public Stopwatch() {
+		this.start = System.currentTimeMillis();
+	}
+
+	public double elapsedTime() {
+		long now = System.currentTimeMillis();
+		return (now - start) / 1000.0;
+	}
+}
